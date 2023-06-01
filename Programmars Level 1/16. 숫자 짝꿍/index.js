1a968fe0b56136a1bf53d91a96e2655c691d4df9
Yena-Yun <cheryleduart@gmail.com>
2023-06-01 11:46:08 +0900

    숫자 짝꿍 (해쉬)

---
+++ Programmars Level 1/16. 숫자 짝꿍/index.js
@@ -0,0 +1,15 @@
+function solution(X, Y) {
+  let answer = '';
+    
+  const hashX = new Array(10).fill(0);
+  const hashY = new Array(10).fill(0);
+    
+  X.split('').forEach((item) => hashX[item]++);
+  Y.split('').forEach((item) => hashY[item]++);
+    
+  hashX.forEach((x, i) => {
+    if (x && hashY[i]) {
+      answer += String(i).repeat(Math.min(hashX[i], hashY[i]))
+    }
+  });
+}
