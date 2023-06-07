e46390d2acb174173e46524fee1098877b0d176b
Yena-Yun <cheryleduart@gmail.com>
2023-06-07 13:42:56 +0900

    달리기 경주 다시 풀기

--- Programmars Level 1/01. 달리기 경주/index.js
+++ Programmars Level 1/01. 달리기 경주/index.js
@@ -1,32 +1,40 @@
 let playerInfo = {};
 
 function getPlayerInfo(players) {
-  // 객체가 필요한 거라서 새로운 배열이 필요하지 않음 => map 대신 forEach 사용
-  players.forEach((player, id) => {
-    playerInfo[player] = id;
-  });
+  players.forEach((player, index) => {
+    playerInfo[player] = index
+  })
 }
 
 function solution(players, callings) {
+  let answer = [];
+  
+  // 1. 선수 이름에 맞는 현재 등수가 저장된 playerInfo 객체를 만든다.
   getPlayerInfo(players);
-  console.log(playerInfo); // { mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4 }
 
+  // 2. 불린 선수, 불린 선수의 현재 등수, 불린 선수의 앞 선수를 구한다.
   callings.forEach((calling) => {
-    // 객체의 key로 바로 선택 -> 배열 순환 필요하지 않음
-    const index = playerInfo[calling]; // 불린 선수의 현재 등수
+    // 불린 선수 = calling
 
-    const frontPlayerName = players[index - 1]; // 구한 인덱스로 players 배열 중 앞 사람 선택
+    // 불린 선수의 현재 등수
+    let index = playerInfo[calling];
 
-    // 배열에서 index로 선택해서 서로 자리 교환 (slice나 splice 메서드 불필요)
+    // 불린 선수의 앞 선수
+    let frontPlayer = players[index - 1];
+
+    // 3. 불린 선수와 앞 선수의 등수를 교체한다.
+    // 배열에 반영
     players[index - 1] = calling;
-    players[index] = frontPlayerName;
+    players[index] = frontPlayer;
 
-    // 등수가 바뀐 뒤 바뀐 등수를 객체에도 반영 (다음 순회 연산을 올바르게 하기 위해)
-    playerInfo[frontPlayerName] = index;
+    // 객체에 반영
     playerInfo[calling] = index - 1;
+    playerInfo[frontPlayer] = index
   });
 
-  return players;
+  answer = players;
+
+  return answer;
 }
 
 const players = ['mumu', 'soe', 'poe', 'kai', 'mine'];
