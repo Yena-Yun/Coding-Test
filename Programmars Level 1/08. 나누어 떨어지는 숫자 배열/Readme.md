7c586dec05300bd29cf3af3b864c2b846b46a0d9
Yena-Yun <cheryleduart@gmail.com>
2023-06-08 16:19:10 +0900

    나누어 떨어지는 숫자 배열 다시 풀이

--- Programmars Level 1/08. 나누어 떨어지는 숫자 배열/Readme.md
+++ Programmars Level 1/08. 나누어 떨어지는 숫자 배열/Readme.md
@@ -5,23 +5,16 @@
 array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
 divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
 
-### 제한사항
-
-arr은 자연수를 담은 배열입니다.
-정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
-divisor는 자연수입니다.
-array는 길이 1 이상인 배열입니다.
-
 ### 풀이 과정
 
-- [원래 풀이]
+원래 풀이
 
 1. result 배열 선언
 2. for-of문으로 돌면서 꺼낸 숫자를 divisor로 나눈 나머지가 0이면 (= 나누어 떨어지면) result 배열에 추가
 3. for문 밖에서 result 길이가 0이면 answer에 -1 아니면 result를 오름차순 정렬한 값 추가
 
-- [새 풀이]
-  filter와 삼항 연산자(ternary operator) 사용
+새 풀이
+요약: filter 메서드와 리턴문에 삼항 연산자 사용
 
 1. arr의 숫자가 divisor로 나누어 떨어지는 조건으로 filter한 새 배열로 answer 교체
 2. answer 길이가 0이면 ? [-1] 반환 : answer 배열을 오름차순 정렬하여 반환
--- Programmars Level 1/08. 나누어 떨어지는 숫자 배열/withFilterAndTernary.js
+++ Programmars Level 1/08. 나누어 떨어지는 숫자 배열/withFilterAndTernary.js
@@ -1,26 +1,10 @@
 function solution(arr, divisor) {
-  let answer = [];
-  // let result = []; // filter를 쓰면 새 배열을 반환하기 때문에 추가 배열 불필요
-
-  answer = arr.filter((num) => num % divisor === 0);
-
-  // if문을 삼항 연산자로 줄이기
-  // if (result.length === 0) {
-  //   answer.push(result.indexOf(divisor));
-  // } else {
-  //   answer = result.sort((a, b) => a - b);
-  // }
-
-  // return answer;
+  let answer = arr.filter((num) => num % divisor === 0);
 
   return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 
-const arr1 = [5, 9, 7, 10];
-const divisor1 = 5;
-
-const arr2 = [3, 2, 6];
-const divisor2 = 10;
+const arr = [5, 9, 7, 10];
+const divisor = 5;
 
 console.log(solution(arr1, divisor1)); // expected: [ 5, 10 ]
-console.log(solution(arr2, divisor2)); // expected: [ -1 ]
