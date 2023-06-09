0444d1063279ed1c2abb44cb707e3aa8eefd18f5
Yena-Yun <cheryleduart@gmail.com>
2023-06-09 15:15:53 +0900

    최소직사각형 다시 풀이

--- Programmars Level 1/12. 최소직사각형/withOrder.js
+++ Programmars Level 1/12. 최소직사각형/withOrder.js
@@ -1,9 +1,9 @@
 function solution(sizes) {
-  const rotated = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
+  const newSizes = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
 
   let maxSize = [0, 0];
   
-  rotated.forEach(([w, h]) => {
+  newSizes.forEach(([w, h]) => {
     if (w > maxSize[0]) maxSize[0] = w;
     if (h > maxSize[1]) maxSize[1] = h;
   });
