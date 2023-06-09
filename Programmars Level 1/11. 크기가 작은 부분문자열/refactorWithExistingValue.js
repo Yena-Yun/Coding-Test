574082b77e2154e78bb45c93ec38e710a1827ad1
Yena-Yun <cheryleduart@gmail.com>
2023-06-09 14:40:35 +0900

    크기가 작은 부분문자열 다시 풀이

--- Programmars Level 1/11. 크기가 작은 부분문자열/refactorWithExistingValue.js
+++ Programmars Level 1/11. 크기가 작은 부분문자열/refactorWithExistingValue.js
@@ -2,7 +2,7 @@ function solution(t, p) {
   let answer = 0;
 
   for (let i = 0; i < t.length - p.length + 1; i++) {
-      const picked = t.slice(i, i + p.length);
+      const picked = t.slice(i, p.length + i);
 
       if (parseInt(picked) <= parseInt(p)) answer++;
   }
