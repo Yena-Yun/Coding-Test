6d2b69e0815e1cc408d25332c5caceb706414eb3
Yena-Yun <cheryleduart@gmail.com>
2023-06-09 15:35:59 +0900

    콜라 문제 다시 풀이

--- Programmars Level 1/13. 콜라 문제/index.js
+++ Programmars Level 1/13. 콜라 문제/index.js
@@ -1,9 +1,13 @@
 function solution(a, b, n) {
-  let answer = 0; // 받은 콜라 수
+  let answer = 0; // 총 받은 콜라 수
 
+  // a가 n보다 커지면 종료
   while (n >= a) {
-    answer += Math.floor(n / a) * b;
-    n = Math.floor(n / a) * b + (n % a);
+    let curCola = Math.floor(n / a) * b; // 현재 차수에 받은 콜라 수
+
+    answer += curCola; // 총 받은 콜라 수(반환할 값)에 누적
+
+    n = curCola * b + (n % a); // n 업데이트 (받아온 콜라 수 + 가져가지 않은 나머지 콜라 수)
   }
 
   return answer;
